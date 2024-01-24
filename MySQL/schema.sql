3da36a51614b618710be13d74af68cc39ee7fb9c
unknown <vanzandt.school.email@gmail.com>
2024-01-24 16:56:17 -0500

    Updated tables on MySQL.

--- MySQL/schema.sql
+++ MySQL/schema.sql
@@ -8,18 +8,16 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE roles(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10,2),
-    department_id INT
-    -- FOREIGN KEY (department_id)
-    -- REFERENCES departments(id)
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT NOT NULL
+    title VARCHAR(30),
+    managers VARCHAR(30)
 );
