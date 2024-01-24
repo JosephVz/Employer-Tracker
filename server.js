1b5339cb90853b65ecc41826c4f69f4203194d87
unknown <vanzandt.school.email@gmail.com>
2024-01-24 14:46:57 -0500

    Added prompt of questions.

---
+++ server.js
@@ -0,0 +1,53 @@
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: 3306,
+    database: 'employeeTracker_db',
+});
+
+function go() {
+    inquirer
+        .createPromptModule({
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department',
+            ],
+        })
+        .then((answer) => {
+            switch (answer.choices) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+            }
+        });
+}
